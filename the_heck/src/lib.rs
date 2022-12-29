68b075404e08c5fd3b5239999df4887a3f0fd663
Elizabeth Oda <efeldeverd@gmail.com>
2022-12-29 13:25:29 +0900

    feat: outline main functions

---
+++ the_heck/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn fix_command() {
+    // Fixes the previous comman when 'theheck' is called.
+}
+
+pub fn get_raw_command () {
+    // Gets the raw command from the command line.
+}
+
+pub fn check_known_args () {
+    // Checks whether the argument is known and can be handled by theheck.
+}
+
+pub fn return_fixed_command () {
+    // Returns the fixed command to the shell.
+}
---
+++ the_heck/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // Parse arguments
+    // Check whether arg is in known args
+    // Fix command
+    // Return fix to shell
+    println!("Hello world!")
+}
