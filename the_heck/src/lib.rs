efae3130ed637e8cd9646d9c96a87f9506e20b6a
Elizabeth Oda <elizabeth@braid.tech>
2023-02-10 14:50:13 +0900

    feat: add fuzzy search to current programs

--- the_heck/src/lib.rs
+++ the_heck/src/lib.rs
@@ -1,19 +1,23 @@
 mod rules;
 
-pub fn fix_from_known_programs(split_last_command: Vec<String>) -> String{
+pub fn fix_from_known_programs(split_last_command: Vec<String>) -> Vec<String>{
     // Checks whether the command contains calls a program known to the-heck
-    let mut program_name = &split_last_command[0];
+    let program_name = &split_last_command[0];
     let program_name = program_name.to_owned();
     println!("Program name: {}", program_name);
 
+    let wrong_command = &split_last_command[1];
+    let wrong_command = wrong_command.to_owned();
+    println!("Wrong command: {}", wrong_command);
+
     let fixed_command = match program_name.as_ref() {
-        "git" => rules::fix_git(),
-        "sl" => rules::fix_ls(),
-        "cargo" => rules::fix_cargo(),
-        _ => ("Not implemented yet.").to_string(),
+        "git" => rules::fix_git(&wrong_command),
+        "sl" => rules::fix_ls(&wrong_command),
+        "cargo" => rules::fix_cargo(&wrong_command),
+        _ => rules::not_implemented(&wrong_command),
     };
     
-    fixed_command.to_string()
+    fixed_command.expect("Failed to return fixed command as a string.")
 
 }
 
--- the_heck/src/rules.rs
+++ the_heck/src/rules.rs
@@ -1,20 +1,37 @@
-use fst::{IntoStreamer, Streamer, Set};
+use fst::{IntoStreamer, Set};
 use fst::automaton::Levenshtein;
 
 
-pub fn fix_git() -> String {
-    let git_keys = vec!["add", "rm", "restore", "restore --staged", "status"];
-    let correct_command = "git";
-    correct_command.to_string()
+pub fn fix_git(wrong_command: &str) -> Result<Vec<String>, Box<dyn std::error::Error>>  {
+    let keys = vec!["add", "restore", "restore --staged", "rm", "status"];
+    let set = Set::from_iter(keys)?;
+    let lev = Levenshtein::new(wrong_command, 1)?;
+    let stream = set.search(lev).into_stream();
+    let keys = stream.into_strs()?;
+    Ok(keys)
 }
 
-pub fn fix_ls() -> String {
-    let correct_command = "ls";
-    correct_command.to_string()
+pub fn fix_ls(wrong_command: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let keys = vec!["ls"];
+    let set = Set::from_iter(keys)?;
+    let lev = Levenshtein::new(wrong_command, 1)?;
+    let stream = set.search(lev).into_stream();
+    let keys = stream.into_strs()?;
+    Ok(keys)
 }
 
-pub fn fix_cargo() -> String {
-    let cargo_keys = vec!["run", "build", "test", "install", "uninstall"];
-    let correct_command = "Hi, Cargo";
-    correct_command.to_string()
+pub fn fix_cargo(wrong_command: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let keys = vec!["build", "install", "run", "test", "uninstall"];
+    let set = Set::from_iter(keys)?;
+    let lev = Levenshtein::new(wrong_command, 1)?;
+    let stream = set.search(lev).into_stream();
+    let keys = stream.into_strs()?;
+    Ok(keys)
+}
+
+pub fn not_implemented(wrong_command: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let unknown_command_msg = format!("{} is not implemented yet", wrong_command);
+    let unknown_command = vec![unknown_command_msg];
+
+    Ok(unknown_command)
 }
